6d7427f2599f0d3602c3e2b6e1c6b2e0353b599a
Scott Sutherland <scott@webcheddar.ca>
2020-07-26 16:33:40 -0400

    SSL error fix

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -35,4 +37,4 @@ app.listen(process.env.PORT || 3000, () => {
 	console.log(`shit works on port ${process.env.PORT}`);
 })
 
-console.log(PORT)
+
